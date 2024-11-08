de1842d2380161e695de6c1c8b8298257ebe6c9e
torihh <142331175+torihh@users.noreply.github.com>
2024-11-08 11:10:49 -0300

    imc.html

---
+++ imc.js
@@ -0,0 +1,11 @@
+let peso = parseFloat(prompt("Informe seu peso:"))
+let altura = parseFloat(prompt)("Informe sua altura:"))
+
+if(isNaN)(peso)||isNaN(altura)||peso<=0||altura<=0){
+    document.write("Por favor informe valores validos!!!");
+}
+else{
+    let.imc=(peso)/
+
+
+}
